6435491f622d34220a419bb42c336d3dbb9ab268
Orpira <orpira@gmail.com>
2026-04-06 12:32:15 +0200

    fix: ajusta rutas base para despliegue en vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
+const isVercel = process.env.VERCEL === "1";
+
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  site: "https://orpira.github.io",
-  base: "/orpira-portfolio",
+	integrations: [tailwind(), react()],
+	site: "https://orpira.github.io",
+	base: isVercel ? "/" : "/orpira-portfolio",
 });
