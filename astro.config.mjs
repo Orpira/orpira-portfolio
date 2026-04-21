f3224916d11fbcdae943d1ec88d385bc489fb4c7
Orlando Pineda Raad <156238036+Orpira@users.noreply.github.com>
2026-04-21 23:01:37 +0000

    fix: base '/' en astro.config.mjs para estilos en Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 
+
 const isVercel = process.env.VERCEL === "1";
 const isProd = process.env.NODE_ENV === "production";
-const siteUrl =
-  process.env.SITE_URL ||
-  (isVercel ? "https://orpira.es" : "https://orpira.github.io");
+const siteUrl = process.env.SITE_URL || "https://orpira.es";
 
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: siteUrl,
-  base: isProd || isVercel ? "/orpira-portfolio" : "/",
+  base: "/",
 });
